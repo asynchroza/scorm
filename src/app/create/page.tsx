37efdcbdc667f634fb0d57d16e85bb05683081ef
Michael Bozhilov <mbozhilov@quickbase.com>
2024-01-18 18:10:30 +0200

    Add colorful display and create pages

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,3 +1,9 @@
 export default function Create() {
-    return <h1>HELLO WORLD</h1>
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+            <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+                <p>HELLO WORLD</p>
+            </div>
+        </main>)
 }
+
