409d89ce153cb4f5a988cb2e4150c463fa896bd3
Michael Bozhilov <mbozhilov@quickbase.com>
2024-01-18 19:57:21 +0200

    Add checks for zip file submission

--- src/app/api/courses/route.ts
+++ src/app/api/courses/route.ts
@@ -1,7 +1,39 @@
-export async function POST(request: Request): Promise<Response> {
-    const blob = await request.blob();
-  
-    console.log(blob);
-  
-    return new Response();
+import { createReadStream } from "fs";
+import { writeFile } from "fs/promises";
+import { NextResponse } from "next/server";
+import { Extract } from 'unzipper';
+
+export async function POST(request: Request) {
+    const data = await request.formData()
+    const file: File | null = data.get('file') as unknown as File
+
+    if (!file) {
+        return NextResponse.json({ error: "No file was provided" }, { status: 400 });
+    }
+
+    if (!file.name.includes('.zip')) {
+        // TODO: think of a better way to validate contents
+        return NextResponse.json({ error: "Provided file is not a zip archive" }, { status: 400 });
+    }
+
+    const bytes = await file.arrayBuffer();
+    const buffer = Buffer.from(bytes);
+
+    const path = `/tmp/${file.name}`
+    await writeFile(path, buffer);
+
+    const readStream = createReadStream(path);
+    const extractionPath = `/tmp/extracted-${file.name}`
+
+    readStream.pipe(Extract({ path: extractionPath }))
+        .on('error', (err) => {
+            return NextResponse.json({ error: err.message }, { status: 500 })
+        });
+
+    // TODO: verify file is of type zip
+    // TODO: unarchive and check contents
+    console.log(`open ${path} to see the uploaded file`)
+
+
+    return NextResponse.json({ message: `Course was successfully uploaded` });
 }
--- src/app/create/components/UploadBox.tsx
+++ src/app/create/components/UploadBox.tsx
@@ -6,6 +6,11 @@ import { Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
 import { type toast as ToastType, useToast } from "~/components/ui/use-toast";
 
+type APIResponse = {
+    error?: string,
+    message?: string
+}
+
 async function handleUpload(file: File | undefined, toast: typeof ToastType) {
     if (!file) {
         toast({
@@ -14,26 +19,25 @@ async function handleUpload(file: File | undefined, toast: typeof ToastType) {
         return;
     }
 
-    console.log(file.type);
-
     try {
+        const data = new FormData()
+        data.set('file', file);
+
         const response = await fetch("/api/courses", {
             method: "POST",
-            body: file,
-            headers: {
-                "Content-Type": file.type,
-            },
+            body: data,
         });
 
         if (!response.ok) {
-            throw new Error("Failed to upload file");
+            toast({
+                title: `File upload failed: ${(await response.json() as APIResponse).error ?? "Unkown reason"}`
+            })
+        } else {
+            toast({
+                title: "File uploaded successfully!",
+            });
         }
-
-        toast({
-            title: "File uploaded successfully!",
-        });
     } catch (error) {
-        console.error("Upload failed:", (error as Error).message);
         toast({
             title: "File upload failed. Please try again later.",
         });
@@ -41,7 +45,7 @@ async function handleUpload(file: File | undefined, toast: typeof ToastType) {
 }
 
 export function UploadBox() {
-    const {toast} = useToast()
+    const { toast } = useToast()
     const [file, setFile] = useState<File>();
 
     return (
@@ -50,6 +54,7 @@ export function UploadBox() {
             <Input
                 id="course"
                 type="file"
+                accept="zip"
                 className="text-black bg-white"
                 onChange={(e) => {
                     if (e.target.files) setFile(e.target.files[0]);
