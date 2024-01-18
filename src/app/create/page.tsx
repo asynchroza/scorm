7be1a327a5e4934e40484d7edc0ffcb2692c298e
Michael Bozhilov <mbozhilov@quickbase.com>
2024-01-18 18:27:49 +0200

    Add upload box

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,11 +1,11 @@
-import { CourseSelect } from "../display/components/SelectCourse";
+import { UploadBox } from "./components/UploadBox";
 
 export default function Create() {
     // fetch list of courses and pass it to CourseSelect
     return (
         <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
             <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-                HELLO WORLD
+                <UploadBox/>
             </div>
         </main>)
 }
